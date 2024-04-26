23d86c9e1b6823015d5c8d8238c83222ec30e2cc
LaoeGaoci <151761910+LaoeGaoci@users.noreply.github.com>
2024-04-26 06:09:03 +0000

    update

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,17 +3,20 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[rustfmt::skip]
+
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
+
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
+
 
 fn main() {
     my_macro!();
